5a67b4397fa3401701d1c6ac6c24bf62ce9bef78
angelapaganucci <angelapaganucci@hotmail.com>
2020-12-03 01:04:35 -0300

    Jogo cobrinha

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -13,12 +14,24 @@ function criarBG() {
 }
 
 function criarCobrinha() {
-    for(i=0; i< snake.length; i++) {
+    for(i=0; i < snake.length; i++) {
         context.fillStyle="green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snake
+}
+
+let jogo = setInterval(iniciarJogo, 100);
+
